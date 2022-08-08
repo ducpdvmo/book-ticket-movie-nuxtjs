db7de94ff8d6080b966679bcdb1bd3f4e76cdb4e
ducpdvmo <phamdinhduc0710@gmail.com>
2022-08-08 19:42:27 +0700

    upload code base

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
