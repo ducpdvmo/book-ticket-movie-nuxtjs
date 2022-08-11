4ed18a0a32eff5340bbde9f004789e136a60f092
ducpdvmo <phamdinhduc0710@gmail.com>
2022-08-11 09:40:17 +0700

    render movies from firebase with slick carousel

---
+++ components/Movie.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <nuxt-link :to="`/Movies/${movie.id}`" class="relative">
+      <img
+        class="w-[285px] h-[437px]"
+        :src="movie.photoUrl"
+        :alt="movie.name"
+      />
+      <div
+        class="absolute flex justify-center items-start ml-[30px] mb-[20px] flex-col w-full bottom-0 left-0 text-white"
+      >
+        <p>{{ movie.tags }}</p>
+        <p>{{ movie.name }}</p>
+        <p>{{ movie.rating }}/10</p>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    movies() {
+      return this.$store.getters['movies/movies']
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ store/movies.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+export const state = () => ({
+  movies: [],
+})
+export const getters = {
+  movies(state) {
+    return state.movies
+  },
+}
+export const mutations = {
+  setMovies(state, payload) {
+    state.movies = payload
+  },
+}
+export const actions = {
+  getAllMovies(vuexContext) {
+    return axios
+      .get('https://nuxt-f6-2ndproject-default-rtdb.firebaseio.com/Movies.json')
+      .then((response) => {
+        const movies = []
+        for (const key in response.data) {
+          movies.push({ ...response.data[key], id: key })
+        }
+        vuexContext.commit('setMovies', movies)
+      })
+  },
+  setMovies(context, movies) {
+    context.commit('setMovies', movies)
+  },
+}
